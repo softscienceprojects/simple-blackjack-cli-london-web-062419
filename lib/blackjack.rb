b4573bf108ff3e2263a5d3f859cf5f42b5cb9cf8
erin johnson <erin.nicole.johnson@gmail.com>
2019-05-12 13:18:05 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,51 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+ rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  input = gets.chomp
+  input
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  card_total = deal_card + deal_card
+  display_card_total(card_total)
+  card_total
 end
 
-def hit?
-  # code hit? here
+def hit?(card_total)
+  prompt_user  #prompt
+  user_input = get_user_input  #store the value of their 'gets'
+  while user_input != "h" && user_input != "s" #whilst does not get h or s
+    invalid_command #error
+    prompt_user #prompt again
+    user_input = get_user_input #reset the value of their gets
+  end
+  if user_input == "h" #if gets h
+    card_total += deal_card #update the card total by adding on a new deal_card
+    display_card_total(card_total)
+  end
+  return card_total # return the card total (outside of everything)
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
@@ -39,6 +53,10 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  card_total = initial_round #assign the card total to the initial_round
+  while card_total < 21 #whilst less than 21
+    card_total = hit?(card_total) #continue reassignign card total to hit
+  end
+ end_game(card_total)
 end
-    
