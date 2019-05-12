919fe1d00ed6533b4999dda17c2e0ce2ee33fa68
erin johnson <erin.nicole.johnson@gmail.com>
2019-05-12 13:06:01 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -56,8 +56,8 @@ def runner
   welcome
   card_total = initial_round
   hit?(card_total)
-  until card_total > 21
+  while card_total < 21
     hit?(card_total)
   end
-end_game(card_total)
+ end_game(card_total)
 end
